c147d16973f20025669bf6040f98e3ec645563a5
katagaki <68231035+katagaki@users.noreply.github.com>
2022-05-08 21:30:02 +0800

    Initial Commit

---
+++ WebViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewer
+//
+//  Created by 堅書 on 2022/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
